1cf0a24eacc6189ef85188f19648ef32d5f0ba88
Radheshyam Nazarkar <radheshyam.nazarkar@gmail.com>
2024-09-19 14:57:22 +0530

    Refactor

--- src/components/ContentWrap.jsx
+++ src/components/ContentWrap.jsx
@@ -4,18 +4,19 @@ import { filterMoviesByLang } from "../utils";
 import MoviesByLanguage from "./MoviesByLanguage";
 import NewArrivals from "./NewArrivals";
 
+const moviesByLanges = ["Hindi", "English", "Spanish"];
+
 const ContentWrap = () => {
   return (
     <div className="content-wrap">
       <NewArrivals newArrivals={movies} />
-      <MoviesByLanguage
-        languageName="Hindi"
-        movies={filterMoviesByLang("Hindi", movies)}
-      />
-      <MoviesByLanguage
-        languageName="English"
-        movies={filterMoviesByLang("English", movies)}
-      />
+      {moviesByLanges.map((lang, index) => (
+        <MoviesByLanguage
+          key={index}
+          languageName={lang}
+          movies={filterMoviesByLang(lang, movies)}
+        />
+      ))}
     </div>
   );
 };
--- src/constants/movies.js
+++ src/constants/movies.js
@@ -8,7 +8,7 @@ const movies = [{
 }, {
   id: 2,
   name: 'Spider Man',
-  languages: ['Hindi'],
+  languages: ['Hindi', 'Spanish'],
   screenTime: "3:05",
   videoUrl: 'https://www.youtube.com/watch?v=AvaAHujmR3A',
   thumbnailUrl: 'https://plus.unsplash.com/premium_photo-1682125795272-4b81d19ea386?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW92aWV8ZW58MHx8MHx8fDA%3D'
